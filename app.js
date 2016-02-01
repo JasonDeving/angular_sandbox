85fd0e9a8b8124465010211525107907c8cbb86e
Jason Chan <aznj50@hotmail.com>
2016-01-31 16:21:00 -0800

    services added and $watch

--- app.js
+++ app.js
@@ -19,14 +19,40 @@ myApp.config(function($routeProvider){
     })
 });
 
-// controllers
-myApp.controller('mainController', ['$scope', '$filter', '$http', '$location', '$log',function($scope, $filter, $http, $location, $log) {
+// service
+myApp.service('nameService', function(){
     
-    $scope.name = "jason";
+    var self  = this;
+    this.name = 'John Doe';
+    
+    this.namelength = function(){
+        
+        return self.name.length;
+    };
+    
+});
+
+// controllers
+myApp.controller('mainController', ['$scope', '$filter', '$http', '$location', '$log', 'nameService', function($scope, $filter, $http, $location, $log, nameService) {
+    
+    $scope.name = nameService.name;
+    // adding watch saves the singleton to changing pages
+    $scope.$watch('name', function() {
+       nameService.name = $scope.name; 
+    });
+    
+    $log.log(nameService.name);
+    $log.log(nameService.namelength());
 }]);
-myApp.controller('secondController', ['$scope', '$filter', '$routeParams',function($scope, $filter, $routeParams) {
+myApp.controller('secondController', ['$scope', '$filter', '$routeParams', 'nameService',function($scope, $filter, $routeParams, nameService) {
+    
     
     $scope.num = $routeParams.num || 1;
+    $scope.name = nameService.name;
+    
+    $scope.$watch('name', function() {
+       nameService.name = $scope.name; 
+    });
 }]);
 
 
